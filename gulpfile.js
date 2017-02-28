22ebe38bb325bb519db8a30c7c5a766be533bc87
wujinzhe <1037680926@qq.com>
2017-02-28 09:17:17 +0800

    add broswerAsync

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,30 @@
 var gulp = require('gulp');
-var contentIncluder = require('gulp-content-includer');
-var rename = require('gulp-rename');
-
+// var contentIncluder = require('gulp-content-includer');
+// var rename = require('gulp-rename');
+var browserSync = require('browser-sync').create();
+var nodemon = require('gulp-nodemon');
 gulp.task('concat',function() {
 	gulp.src("./tpl/*.html")
 		.pipe(contentIncluder({
 			includerReg:/<!\-\-include\s+virtual\="([^"]+)"\s*\-\->/g
 		}))
 		.pipe(gulp.dest('./'));
+});
+
+gulp.task('server',function(){
+    nodemon({
+        script:'app.js',
+        ignore:["gulpfile.js","node_mondules/"],
+        env:{
+            'NODE_ENV':'development'
+        }
+    }).on('start',function(){
+        browserSync.init({
+            proxy:'http://localhost:3000',
+            files:["public/**/*.*","views/**","views/**/*.*","routes/**"],
+            port:8080
+        },function(){
+            console.log('browser refreshed');
+        });
+    });
 });
