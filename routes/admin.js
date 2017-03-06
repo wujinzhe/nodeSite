05bb64c818d2d253d3b24465404706dda7a8b9c1
wujinzhe <1037680926@qq.com>
2017-03-06 09:49:51 +0800

    添加公共模板

--- routes/admin.js
+++ routes/admin.js
@@ -17,4 +17,40 @@ router.get('/login',function(req, res, next){
     });
 });
 
+router.get('/userCenter',function(req, res, next){
+    res.render('admin/userCenter',{
+        title:'用户中心页面'
+    });
+});
+
+router.get('/seo',function(req, res, next){
+    res.render('admin/seo',{
+        title:'SEO优化'
+    });
+});
+
+router.get('/new',function(req, res, next){
+    res.render('admin/new',{
+        title:'新闻管理'
+    });
+});
+
+router.get('/download',function(req, res, next){
+    res.render('admin/download',{
+        title:'应用下载'
+    });
+});
+
+router.get('/job',function(req, res, next){
+    res.render('admin/job',{
+        title:'招聘信息'
+    });
+});
+
+router.get('/banner',function(req, res, next){
+    res.render('admin/banner',{
+        title:'首页banner设置'
+    });
+});
+
 module.exports = router;
